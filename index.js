37c3cf64aad0754b1991b69f7d14ad52dba485f3
Tarikulk <ullahtarik300@gmail.com>
2022-10-26 00:29:07 +0600

    backend all set up done with data categories

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const cors = require("cors")
 const port = process.env.PORT || 5000;
 
 app.use(cors());
+
 const categories = require("./data/categories.json");
+const courses = require("./data/courses.json");
 
 app.get('/', (req, res) =>{
    res.send("Courses API running")
@@ -14,6 +16,20 @@ app.get('/', (req, res) =>{
 app.get("/course-categories", (req, res) =>{
     res.send(categories);
 });
+
+
+app.get("/course/:id", (req, res) =>{
+    const id = req.params.id;
+    const selectedCourse = courses.find(course => course._id === id);
+    res.send(selectedCourse);
+});
+
+
+app.get("/category/:id", (req, res) =>{
+    const id = req.params.id;
+    const categoryCourse = courses.filter(category => category.category_id === id);
+    res.send(categoryCourse);
+})
 
 
 
