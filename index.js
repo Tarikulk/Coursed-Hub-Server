6adf7bb4a589a9f3ebadd6709ab244d8640ece42
Tarikulk <ullahtarik300@gmail.com>
2022-10-25 23:58:23 +0600

    courses api running in the port 5000 successfully

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+app.get('/', (req, res) =>{
+   res.send("Courses API running")
+});
+
+
+app.listen(port, () =>{
+    console.log('Courses-Hub server running on port,', port)
+});
